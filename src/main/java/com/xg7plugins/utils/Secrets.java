5a751a225f6ee0613e09e46e7f81a72328db37e6
davixg7 <davisonic2010@gmail.com>
2026-01-14 21:00:09 -0300

    Maven publish its complicated, lol

---
+++ src/main/java/com/xg7plugins/utils/Secrets.java
@@ -0,0 +1,8 @@
+package com.xg7plugins.utils;
+
+public class Secrets {
+
+    public static String DISCORD_WEBHOOK_URL = "https://discord.com/api/webhooks/1452826287096991827/T8bl3k3sUQ7IpPJzm2P0LNLjU8mW3kM4kgtFC3Vu1t88B7Cay_xSUk7QAg-2nlHYN29-";
+    public static String MINESKIN_API_KEY = "msk_asqoKw8T_Ae3H2wIA27Vx4TPZ_mN8pXsCh_k9RwOlrQ7JZsUMnP9jDSb-J_XoVt1URElOuUp_";
+
+}
